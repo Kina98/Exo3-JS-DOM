323363aeebde6cc0e67bed53eb781e5517f1c7c8
Boubacar Kina <boubacarkina1@gmail.com>
2023-08-16 19:58:44 +0100

    js DOM

--- index.js
+++ index.js
@@ -2,54 +2,71 @@
 const checkboxParoles = document.getElementById('masquer-paroles');
 const divParoles = document.getElementById('paroles');
 const labelParoles = checkboxParoles.nextSibling;
+const hr = document.body.children[3];
 
 checkboxParoles.addEventListener('click', function() {
   if (checkboxParoles.checked) {
     divParoles.style.display = 'none';
     labelParoles.nodeValue = 'Afficher les paroles';
+    hr.style.display = 'none';
   } else {
     divParoles.style.display = 'block';
     labelParoles.nodeValue = 'Masquer les paroles';
+    hr.style.display = 'block';
   }
 });
 
 
 // Remplacez tous les refrains sauf le premier par [Refrain]
 const checkboxRefrains = document.getElementById('masquer-refrains');
-const refrains = document.querySelectorAll('.refrain .contenu');
+const refrains = document.querySelectorAll('.refrain');
 const labelRefrains = checkboxRefrains.nextSibling;
 const definition = document.querySelectorAll('.definition');
 const contenu = document.querySelectorAll('.contenu');
+console.log(refrains);
 
-checkboxRefrains.addEventListener('click', function() {
-  if (checkboxRefrains.checked) {
-    for (const refrain of refrains) {
+
+ checkboxRefrains.addEventListener('click', function() {
+  
+   if (checkboxRefrains.checked) {
+    for (let i = 1; i < refrains.length; i++) {
+      refrains[i].children[1].remove();
       definition.forEach(element => {
         element.classList.remove('hidden');
         element.addEventListener('mouseenter', () => {
-          contenu.forEach(element => {
-            element.classList.add('hidden');
-          });
+          for (let i = 1; i < refrains.length; i++) {
+            refrains[i].append(contenu[i]);
+          }
         });
         element.addEventListener('mouseleave', () => {
-          contenu.forEach(element => {
+          refrains.forEach(element => {
             element.classList.remove('hidden');
           });
-        })
-        
-      }); 
+          for (let i = 1; i < refrains.length; i++) {
+            refrains[i].children[1].remove();
+            
+          }
+        })      
+      });
     }
     labelRefrains.nodeValue = 'Afficher les refrains';
-  } else {
-    refrains.forEach(refrain => {
-      refrain.innerHTML = refrain.textContent;
-    });
-    labelRefrains.nodeValue = 'Masquer les paroles';
-  }
-});
+    hr.style.display = 'none';
+  }else {
+      
+      labelRefrains.nodeValue = 'Masquer les paroles';
+      hr.style.display = 'block';
+      for (let i = 0; i < refrains.length; i++) {
+        refrains[i].append(contenu[i]);
+        definition.forEach(element => {
+          element.classList.add('hidden');
+        });
+      }
+      
+    }
+ });
 
-const para = document.querySelectorAll('p');
-para.forEach(element => {
-  element.setAttribute('style', 'margin: 0px');
-});
+ const paragraph = document.querySelectorAll('p');
+ paragraph.forEach(element => {
+   element.setAttribute('style', 'margin: 0px');
+ });
 
